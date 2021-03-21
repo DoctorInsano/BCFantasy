7b74ffea6ef0f68a916161421892b03e619ea45c
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2021-03-21 00:20:10 -0700

    cards.py: example

--- common/cards.py
+++ common/cards.py
@@ -1,15 +1,31 @@
-from bcfcc import AddGP
+import random
+from bcfcc import CCCommand, WriteArbitrary, SetStat
 
-class Card(AddGP):
-    def __init__(self, label, cost):
+class Card(WriteArbitrary):
+    def __init__(self, label, cost, tier=None):
         super().__init__(label, cost, None)
-        self._tier = None
+        self._tier = tier
 
-    def sell(self):
-        super().__call__(self.cost)
+    def sell(self, gm):
+        gm.gp += self.cost
 
-    def full(self):
+    def full(self, gm):
         pass
 
-    def direct(self):
+    def direct(self, gm):
         pass
+
+class GainLevel(Card):
+    """
+    Full: 'Level up' a single (random char)
+    Direct: +5 levels to all party (one battle)
+    """
+    def __init__(self):
+        super().__init__("gain_level", 200, 1)
+
+    def full(self, gm):
+        # FIXME: this is only temporary
+        return SetStat(self.label)(slot=random.randint(0, 3))
+
+    def direct(self, gm):
+        return SetStat(self.label)(slot=random.randint(0, 3))
